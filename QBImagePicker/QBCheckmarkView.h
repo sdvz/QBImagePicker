785025923d7e61cfe070a0e913c6e58ae67cf4c8
Sage De Vaz <sagedevaz@gmail.com>
2017-06-26 14:33:15 -0700

    removed border and shadow

--- QBImagePicker/QBCheckmarkView.h
+++ QBImagePicker/QBCheckmarkView.h
@@ -11,10 +11,8 @@
 IB_DESIGNABLE
 @interface QBCheckmarkView : UIView
 
-@property (nonatomic, assign) IBInspectable CGFloat borderWidth;
 @property (nonatomic, assign) IBInspectable CGFloat checkmarkLineWidth;
 
-@property (nonatomic, strong) IBInspectable UIColor *borderColor;
 @property (nonatomic, strong) IBInspectable UIColor *bodyColor;
 @property (nonatomic, strong) IBInspectable UIColor *checkmarkColor;
 
--- QBImagePicker/QBCheckmarkView.m
+++ QBImagePicker/QBCheckmarkView.m
@@ -15,29 +15,18 @@
     [super awakeFromNib];
     
     // Set default values
-    self.borderWidth = 1.0;
     self.checkmarkLineWidth = 1.2;
     
-    self.borderColor = [UIColor whiteColor];
-    self.bodyColor = [UIColor colorWithRed:(20.0 / 255.0) green:(111.0 / 255.0) blue:(223.0 / 255.0) alpha:1.0];
+    self.bodyColor = [UIColor colorWithRed:(79.0 / 255.0) green:(134.0 / 255.0) blue:(236.0 / 255.0) alpha:1.0];
     self.checkmarkColor = [UIColor whiteColor];
-    
-    // Set shadow
-    self.layer.shadowColor = [[UIColor grayColor] CGColor];
-    self.layer.shadowOffset = CGSizeMake(0, 0);
-    self.layer.shadowOpacity = 0.6;
-    self.layer.shadowRadius = 2.0;
 }
 
 - (void)drawRect:(CGRect)rect
 {
-    // Border
-    [self.borderColor setFill];
-    [[UIBezierPath bezierPathWithOvalInRect:self.bounds] fill];
     
     // Body
     [self.bodyColor setFill];
-    [[UIBezierPath bezierPathWithOvalInRect:CGRectInset(self.bounds, self.borderWidth, self.borderWidth)] fill];
+    [[UIBezierPath bezierPathWithOvalInRect:self.bounds] fill];
     
     // Checkmark
     UIBezierPath *checkmarkPath = [UIBezierPath bezierPath];
